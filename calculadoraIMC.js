75cc47c52a949dfe532248ab555d7374894eb461
Rubenro23 <rubenro2331997@gmail.com>
2024-04-11 12:12:18 +0200

    Create calculadoraIMC.js

---
+++ calculadoraIMC.js
@@ -0,0 +1,28 @@
+function calcularIMC(){
+    var peso = parseFloat(document.getElementById('pesoInput').value);
+    var altura = parseFloat(document.getElementById('alturaInput').value);
+
+    if(isNaN(peso) || isNaN(altura)||peso<=0||altura<=0){
+        document.getElementById('resultado').innerText='Por favor,inggresa un peso y una altura';
+        return;
+    }
+
+    var imc=peso/(altura*altura);
+    var resultadoDiv = document.getElementById('resultado');
+
+
+    switch (true) {
+        case (imc<18.5):
+            resultadoDiv.innerText='Estas en el rango de peso Insuficiente'
+            break;
+        case(imc<=24.9):
+            resultadoDiv.innerText='Estas en el rango de peso Saludable'
+            break;
+        case (imc<=29.9):
+            resultadoDiv.innerText='Estas en el rango de peso Sobrepeso'
+            break;
+            default:
+                resultadoDiv.innerText='Estas en el rango de Obesidad'
+            break;
+    }
+}
